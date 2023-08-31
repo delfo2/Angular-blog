93b4e9c6ac1f379d18fcb4a2de7cb2e7998571e8
oi <105376328+delfo2@users.noreply.github.com>
2023-08-31 10:56:06 -0300

    animation of title working

--- src/app/shared/tittle/tittle.component.ts
+++ src/app/shared/tittle/tittle.component.ts
@@ -6,27 +6,69 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 	styleUrls: ['./tittle.component.css'],
 })
 export class TittleComponent implements OnInit {
-	titleText = 'Abobora';
+	titleClass: string = 'broken__title wrong__position';
+	titleText: string = 'Last News';
+	originalWord: string = this.titleText;
+	titleSize: number = this.titleText.length;
+	tempPhrase: string = '';
+	delay = 0;
+	multipliyer = 50;
 
-	ngOnInit() {
+	ngOnInit(): void {
 		this.animateTitleText();
+		setInterval(() => {
+			this.originalWordAnimation();
+		}, this.titleSize * this.multipliyer + 1000);
+
 	}
 
-	animateTitleText() {
-		let originalWord = this.titleText;
-		let tempPhrase = '';
-		const letters = originalWord.split('');
-		let speed = 300;
-		letters.forEach((letter, i) => {
-			setTimeout(() => {
-				this.titleText = tempPhrase;
-				tempPhrase = this.titleText += letter;
-				this.titleText = tempPhrase + '|';
+	animateTitleText(): void {
+		const letters = this.titleText.split('');
 
-			}, i * speed);
+		letters.forEach((letter) => {
+			if (Math.round(Math.random()) % 2 == 0) {
+				this.addNewLetter(letter);
+			} else {
+				this.removeLetter(letter);
+			}
 		});
+	}
+
+	removeLetter(letter: string) {
+		this.delay += this.multipliyer;
 		setTimeout(() => {
-			this.titleText = originalWord;
-		}, letters.length * speed);
+			let text = this.titleText.split('').pop();
+			this.titleText = text != undefined ? text : this.titleText;
+			this.addNewLetter(letter);
+		}, this.delay);
+	}
+	addNewLetter(letter: string): void {
+		this.delay += this.multipliyer;
+		const delayInCreactionTime = this.delay;
+		setTimeout(() => {
+			if (this.delay === delayInCreactionTime) {
+				this.originalWordAnimation();
+			}
+			if (this.delay > this.titleSize * 100) {
+				this.titleClass = 'broken__title right__position';
+			}
+			this.titleText = this.tempPhrase;
+			this.tempPhrase = this.titleText += letter;
+			this.titleText = this.tempPhrase + '▌';
+		}, this.delay);
+	}
+
+	originalWordAnimation() {
+		this.titleClass = 'title right__position';
+		setTimeout(() => {
+			this.putDigitOnEnd();
+		}, 400);
+	}
+
+	putDigitOnEnd(): void {
+		this.titleText = this.originalWord + '▌';
+		setTimeout(() => {
+			this.titleText = this.originalWord;
+		}, 800);
 	}
 }
